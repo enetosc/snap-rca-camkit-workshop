efaa8b4af23c0430c6a3d1fe70a97cca887cf24d
Edgar Neto <eneto@snapchat.com>
2024-03-21 11:24:47 +0000

    improvements to ck web slides

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { bootstrapCameraKit } from '@snap/camera-kit';
 
 (async function () {
   const cameraKit = await bootstrapCameraKit({
-    apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzEwOTQ4MDYwLCJzdWIiOiIyYjdiODEzNC01ZTNjLTQyN2QtYmMyMi1iMjc0OGY3Njc1ZTN-U1RBR0lOR34wMjk0NDc5MS0wYTJhLTRkNDktOWU4Zi01YjFhZThjZDJiMzcifQ.49WSmXKmerSgR2dsx_oIUqtN4YivwufekH2tMvHMd-A',
+    apiToken: '<YOUR API TOKEN>',
   });
   const liveRenderTarget = document.getElementById(
     'canvas'
@@ -16,8 +16,8 @@ import { bootstrapCameraKit } from '@snap/camera-kit';
   await session.play();
 
   const lens = await cameraKit.lensRepository.loadLens(
-    'b0bf4c2d-8367-4b81-a20b-c6b6ca140e28',
-    'cac0081e-73d2-43cb-97b2-311a129ce328'
+    '<YOUR LENS ID>',
+    '<YOUR LENS GROUP ID>'
   );
 
   await session.applyLens(lens);
